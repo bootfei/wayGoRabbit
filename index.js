e87e48622623d900fca40ba04a75688f1fb2efad
bootfei <qifeish@gmail.com>
2023-02-09 14:37:41 +0800

    添加测试和ignore文件

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('欢迎使用微信云托管！')
+  res.send('欢迎使用微信云托管，qf！')
 })
 
 app.get('/test', (req, res) => {
-    res.send('test');
+    res.send('test，qf');
 })
 
 
